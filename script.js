80047132f6d937ea42ebd5ae379408e88f7a9ee2
Karen Langner <krenabeth@yahoo.com>
2016-07-05 17:20:28 -0400

    first ui daily challenge commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var userName = getElementById("userName");
+
+function thanks(userName) {
+	confirm("Thank you for your order " + userName + "! you will recieve an email confirmation shortly.")
+}
+
+
+// var temp = 98;
+
+// var event;
+// var casual = "something comfy";
+// var semi = "a polo or";
+// var formal = "a suit";
+
+// event = formal;
+
+
+// if (temp < 54 && event === true) {
+
+//   console.log("Suggested attire: " + event + " and a coat.");
+// } else if (temp <= 70) {
+//   console.log("Suggested attire: " + event + " and a jacket");
+// } else if (temp > 70) {
+//    console.log("Suggested attire: " + event + " and no jacket");
+// } else {
+//   console.log("I don't know");
+// }
+
